3c85bb690344bb0bea58903446be7216f0197d65
CN\yxlingyongkang <dreammust@163.com>
2023-12-15 17:11:22 +0800

    add file

---
+++ PluginCollection/src/main/java/com/plugin/demo/PluginCollectionApplication.java
@@ -0,0 +1,13 @@
+package com.plugin.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluginCollectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PluginCollectionApplication.class, args);
+    }
+
+}
---
+++ PluginCollection/src/test/java/com/plugin/demo/PluginCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plugin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluginCollectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
