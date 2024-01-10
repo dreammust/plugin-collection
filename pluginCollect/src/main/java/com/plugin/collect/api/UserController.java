09e94eb81ecb921fbe8def4e422c529c66098265
CN\yxlingyongkang <dreammust@163.com>
2024-01-10 15:12:44 +0800

    使用screw生成数据库表结构文档

--- pluginCollect/src/main/java/com/plugin/collect/api/UserController.java
+++ pluginCollect/src/main/java/com/plugin/collect/api/UserController.java
@@ -5,8 +5,6 @@ import com.plugin.collect.model.UserModel;
 import com.plugin.collect.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
